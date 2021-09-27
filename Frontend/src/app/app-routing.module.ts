9deb09dc3b4793713137908895702a259b99023f
Ericson da Fonseca <ericsonf@outlook.com>
2021-09-27 20:01:37 -0300

    Update Angular

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'people' },
-  { path: 'people', loadChildren: './people/people.module#PeopleModule' }
+  { path: 'people', loadChildren: ()=> import('./people/people.module').then(m => m.PeopleModule)}
 ];
 
 @NgModule({
